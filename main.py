79a8264630892a65eb48d480126e4033607eafec
ParasBhavarthi <paras.bhavarthi@gmail.com>
2021-09-26 08:42:55 -0700

    latest version with low average

--- main.py
+++ main.py
@@ -3,62 +3,113 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import math as math
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def get_stock_info(name):
     # Use a breakpoint in the code line below to debug your script.
     stock = yf.Ticker(name)
-    return stock.history(period="1y")
+    return stock.history(period="5y")
+
+def get_change(current, previous):
+    if current == previous:
+        return 0
+    try:
+        return (abs(current - previous) / previous) * 100.0
+    except ZeroDivisionError:
+        return float('inf')
+
 # Press the green button in the gutter to run the script.
-def calculate_dollar_cost_with_min_average_new(df_stock_info, amount):
+def calculate_dollar_cost_with_min_average_new(df_stock_info, amount, is_dividant):
     total_amount = 0
     total_shares = 0
     days_count = 0
     days_uninvested = 0
-
+    days_not_invested = 0
     df_stock_info.index = np.arange(1, len(df) + 1)
+    weekly_budget = amount * 5
+    divident = 0
+    total_dividant = 0
+    count =1
+    five_day_stock = 0
+    five_day_amount = 0
 
     for index, row in df_stock_info.iterrows():
-        closed = row['Close']
+        closed = round(row['Close'],2)
         five_day_avg = row['5_day_avg']
+
+        if row['Dividends'] != 0 and is_dividant:
+            divident += (total_shares * row['Dividends'])
+            total_dividant += divident
+            share2 = divident / closed
+            print("Date of Dividends")
+            print(count)
+            count+=1
+            total_shares += share2
+
+
         if index == 1:
             share = amount / closed
             total_amount += amount
             total_shares += share
-            days_count+=1
+            five_day_stock += share
+            five_day_amount += amount
+            days_count=1
+            weekly_budget -= amount
             #print(row['Date'])
             continue
         if math.isnan(row['5_day_avg']):
             share = amount / closed
             total_amount += amount
             total_shares += share
+            five_day_stock += share
+            five_day_amount += amount
             days_count += 1
-            continue
-        if closed > (row['5_day_avg']):
-            days_uninvested += 1
-            continue
-        if days_uninvested > 4:
-             share = (amount * days_uninvested) / closed
-             total_amount += (amount * days_uninvested)
-             total_shares += share
-             days_uninvested = 0
-             days_count += 5
-
-        if closed < (row['5_day_avg']) :
-            if days_uninvested == 0:
-                days_uninvested = 1
-            share = (amount*days_uninvested) / closed
-            total_amount += (amount*days_uninvested)
-            total_shares += share
-            days_count += days_uninvested
+            weekly_budget -= amount
             days_uninvested = 0
+            continue
 
+        if index % 5 == 0:
+            if weekly_budget > 0:
+                share = (weekly_budget) / closed
+                total_amount += (weekly_budget)
+                total_shares += share
+                days_count += (weekly_budget/amount)
+                days_uninvested = 0
+            five_day_stock = 0
+            five_day_amount = 0
+            weekly_budget = amount * 5
+
+        our_five_day_avg = 0
+        if five_day_stock != 0:
+            our_five_day_avg = five_day_amount / five_day_stock
+
+        if closed > (our_five_day_avg * 1.12):
+            days_uninvested += 1
+            days_not_invested+= 1
+            continue
+        elif weekly_budget > 0:
+            share = amount / closed
+            total_amount += amount
+            total_shares += share
+
+            five_day_stock += share
+            five_day_amount += amount
+            weekly_budget -= amount
+            days_count += 1
+
+            if closed < our_five_day_avg*1.1 and weekly_budget > 0:
+                share = (weekly_budget) / closed
+                total_amount += (weekly_budget)
+                total_shares += share
+                days_count += (weekly_budget/amount)
+                weekly_budget = 0
+                days_uninvested = 0
+
+    print("total_dividant", round(total_dividant, 2))
     print("total_amount", round(total_amount, 2))
     print("total_shares", round(total_shares, 2))
     print("days_count", days_count)
-    print("Price per share ", total_amount / total_shares)
+    print("days_not_invested", days_not_invested)
+    print("Price per share ", (total_amount+total_dividant) / total_shares)
     print("==========================")
 
     return total_amount, total_shares, days_count
@@ -116,6 +167,7 @@ def calculate_dollar_cost_with_min_average2(df_stock_info, amount):
         if closed < (row['5_day_avg']) and monthly_budget > 0:
             if days_uninvested == 0:
                 days_uninvested = 1
+
             share = (amount*days_uninvested) / closed
             total_amount += (amount*days_uninvested)
             total_shares += share
@@ -128,7 +180,7 @@ def calculate_dollar_cost_with_min_average2(df_stock_info, amount):
     print("total_amount", round(total_amount, 2))
     print("total_shares", round(total_shares, 2))
     print("days_count", days_count)
-    print("Price per share ", total_amount / total_shares)
+    print("Price per share ", (total_amount) / total_shares)
     print("==========================")
 
     return total_amount, total_shares, days_count
@@ -138,7 +190,6 @@ def calculate_dollar_cost(df_stock_info, amount):
     total_amount = 0
     total_shares = 0
     days_count = 0
-
     total_dividant = 0
     divident = 0
 
@@ -286,7 +337,7 @@ def divident_reinvestment(df_stock_info, amount):
     print("total_amount divident_reinvestment", round(total_amount,2))
     print("total_shares divident_reinvestment", round(total_shares,2))
     print("days_count divident_reinvestment", days_count)
-    print("Price per share ", total_amount/total_shares )
+    print("Price per share ", (total_amount+total_dividant)/total_shares )
     return total_amount, total_shares, days_count
 
 
@@ -305,40 +356,56 @@ def get_change(current, previous):
     except ZeroDivisionError:
         return float('inf')
 
+
+Input_column = {
+    "ID": int,
+    "Name": str,
+    "Address": str
+}
+
+
 if __name__ == '__main__':
-    df_stock_info = get_stock_info('SPY')
+    df_stock_info = get_stock_info('VICI')
+    amount = 5
     #print(df_stock_info.columns)
     print(df_stock_info)
-
 
     # Convert the dictionary into DataFrame
     pd.set_option("display.max_rows", None, "display.max_columns", None)
     df = pd.DataFrame(df_stock_info, columns=['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
 
-    df = df.iloc[::-1]
-    df['5_day_avg'] = df.Close.rolling(window=5).mean()
 
-    df = df.iloc[::-1]
+    df['5_day_avg'] = round(df.Close.rolling(window=5).mean(), 2)
+    df['closed_p_change'] = df.Close.pct_change(periods=1)
 
-    df.to_csv("vti_1y.csv")
+    max_value = df.closed_p_change.max()
+    min_value = df.closed_p_change.min()
+    mean_value = df.closed_p_change.mean()
+    print(f"max value {max_value}")
+    print(f"min value {min_value}")
+    print(f"mean value {mean_value}")
+
+    #df.to_csv("vti_10y.csv")
+
+    print("last price")
+    print(df['Close'].iloc[-1])
 
     print("calculate_dollar_cost ")
-    total_amount_invested, total_shares, days_count = calculate_dollar_cost(df, 30)
+    total_amount_invested, total_shares, days_count = calculate_dollar_cost(df, amount)
     profit, percent_change = calculate_profit(total_amount_invested, total_shares, df['Close'].iloc[-1])
 
-    print("calculate_dollar_cost_with_min_average ")
-    total_amount_invested, total_shares, days_count = calculate_dollar_cost_with_min_average_new(df, 30)
-    profit, percent_change = calculate_profit(total_amount_invested, total_shares, df['Close'].iloc[-1])
+    #print("calculate_dollar_cost_with_min_average ")
+    #total_amount_invested, total_shares, days_count = calculate_dollar_cost_with_min_average_new(df, 15, True)
+    #profit, percent_change = calculate_profit(total_amount_invested, total_shares, df['Close'].iloc[-1])
 
-    #print("divident_reinvestment")
-    #total_amount, total_shares, days_count = divident_reinvestment(df, 150)
-    #profit1, percent_change2 = calculate_profit(total_amount_invested, total_shares, df['Close'].iloc[-1])
+    print("divident_reinvestment")
+    total_amount, total_shares, days_count = divident_reinvestment(df, amount)
+    profit1, percent_change2 = calculate_profit(total_amount_invested, total_shares, df['Close'].iloc[-1])
 
-    #rint("percent_change direct ", get_change(df['Close'].iloc[-1], df['Close'].iloc[0]))
+    #print("percent_change direct ", get_change(df['Close'].iloc[-1], df['Close'].iloc[0]))
 
     #print("percent_change ", percent_change)
     #print("percent_change ", percent_change2)
-
     #print("calculate_consecutive_low ")
     #total_amount_invested, total_shares, days_count = calculate_consecutive_low(df, 15, 0)
 
